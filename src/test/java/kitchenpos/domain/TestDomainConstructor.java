732e24e0ab0cae0cd2db0e266d8459802db1aa66
hhlyn0328 <hhlyn0328@daum.net>
2021-01-09 20:21:40 +0900

    test: ProductServiceTest 작성

--- src/test/java/kitchenpos/domain/TestDomainConstructor.java
+++ src/test/java/kitchenpos/domain/TestDomainConstructor.java
@@ -3,10 +3,18 @@ package kitchenpos.domain;
 import java.math.BigDecimal;
 
 public class TestDomainConstructor {
-	public static Product product(String name, int price) {
+	public static Product product(String name, Integer price) {
 		Product product = new Product();
 		product.setName(name);
-		product.setPrice(BigDecimal.valueOf(price));
+		if (price != null) {
+			product.setPrice(BigDecimal.valueOf(price));
+		}
+		return product;
+	}
+
+	public static Product productWithId(String name, int price, long id) {
+		Product product = product(name, price);
+		product.setId(id);
 		return product;
 	}
 }
