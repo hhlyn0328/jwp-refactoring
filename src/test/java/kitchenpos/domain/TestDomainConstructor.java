be7d2c64c7f3df0be10af251e654d8688f5c02b7
hhlyn0328 <hhlyn0328@daum.net>
2021-01-09 20:39:37 +0900

    test: MenuGroupRestControllerTest 작성

--- src/test/java/kitchenpos/domain/TestDomainConstructor.java
+++ src/test/java/kitchenpos/domain/TestDomainConstructor.java
@@ -12,9 +12,21 @@ public class TestDomainConstructor {
 		return product;
 	}
 
-	public static Product productWithId(String name, int price, long id) {
+	public static Product productWithId(String name, int price, Long id) {
 		Product product = product(name, price);
 		product.setId(id);
 		return product;
+	}
+
+	public static MenuGroup menuGroup(String name) {
+		MenuGroup menuGroup = new MenuGroup();
+		menuGroup.setName(name);
+		return menuGroup;
+	}
+
+	public static MenuGroup menuGroupWithId(String name, Long Id) {
+		MenuGroup menuGroup = menuGroup(name);
+		menuGroup.setId(Id);
+		return menuGroup;
 	}
 }
